56a34ed9e1838dfd32c855263e7c9774592ee7b1
Boyd Kane <boydrkane@gmail.com>
2019-04-24 21:29:24 +0200

    Add docstring comment to download_urls.py

--- data/download_urls.py
+++ data/download_urls.py
@@ -1,3 +1,13 @@
+"""
+    1. Get a wordnet_id from each line in group_codes.txt,
+    2. Get a page containing urls to images of that wordnet_id
+    3. Put all those urls into image_urls/<wordnet_id>.txt
+    4. Repeat with the next wordnet_id from group_codes.txt
+
+    TODO Autoformat all files
+    TODO Rename Groupcodes.txt
+"""
+
 import urllib.request
 import os
 import sys
@@ -5,16 +15,16 @@ import sys
 file = open(os.path.join(sys.argv[1], 'group_codes.txt'), 'r')
 
 for line in file:
-	code = line.split()[0]
-	print(code)
+    wordnet_id = line.split()[0]
+    print(wordnet_id)
 
-	url_file = open(os.path.join(sys.argv[1], 'image_urls', code+'.txt'), 'w')
+    url_file = open(os.path.join(sys.argv[1], 'image_urls', wordnet_id + '.txt'), 'w')
 
-	response = urllib.request.urlopen('http://image-net.org/api/text/imagenet.synset.geturls?wnid='+code)
-	for response_line in response:
-		url = response_line.decode().strip()
-		if len(url) > 0:
-			url_file.write(url + '\n')
+    response = urllib.request.urlopen('http://image-net.org/api/text/imagenet.synset.geturls?wnid=' + wordnet_id)
+    for response_line in response:
+        url = response_line.decode().strip()
+        if len(url) > 0:
+            url_file.write(url + '\n')
 
-	url_file.close()
+    url_file.close()
 file.close()
