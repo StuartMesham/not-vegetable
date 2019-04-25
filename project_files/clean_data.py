2a788c1efe5913144961afd64bacae21a6ad5a27
Boyd Kane <boydrkane@gmail.com>
2019-04-25 11:09:25 +0200

    Get clean_data.py working from the command line

--- project_files/clean_data.py
+++ project_files/clean_data.py
@@ -1,7 +1,15 @@
 import sys
 
 
+
+
+
+
+def my_function(string):
+    print(string)
+
 # To run from the command line:
-# ```python3 clean_data.py myfunction```
+# python3 clean_data.py my_function my_parameter
+
 if __name__ == '__main__':
     globals()[sys.argv[1]](sys.argv[2])
