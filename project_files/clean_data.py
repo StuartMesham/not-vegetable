289ff06a05b85b61484b58b38c835d39d97bb31c
Boyd Kane <boydrkane@gmail.com>
2019-04-26 08:00:57 +0200

    Finish clean_data.py Start testing clean_data.py

--- project_files/clean_data.py
+++ project_files/clean_data.py
@@ -1,3 +1,9 @@
+"""
+# Usage
+1. Run ```python3 clean_data.py remove_duplicates path```
+    1.2 path =
+
+"""
 import os
 import sys
 import glob
@@ -16,22 +22,27 @@ def remove_duplicates(path: str):
         for line in file:
             if len(line) > 0:
                 url = line.strip()
-                url = file_name[:-4]
-                url_to_wordnet_ids[url].append(url)
+                wordnet_id = file_name[:-4]
+                url_to_wordnet_ids[url].append(wordnet_id)
         file.close()
 
     # Then go through the files again to remove those duplicates
     for url in url_to_wordnet_ids:
         if len(url_to_wordnet_ids[url]) > 1:
-            # Remove all of these files
-            file = open(url + ".txt", "w")
+            for wordnet_id in url_to_wordnet_ids[url]:
+                file = open(wordnet_id + ".txt", "w")
+                lines = file.readlines()
+                lines.remove(url)
+                file.writelines(lines)
+                file.close()
 
 
 def my_function(string):
     print(string)
 
+
 # To run from the command line:
-# python3 clean_data.py my_function my_parameter
+# python3 clean_data.py remove_duplicates path
 
 if __name__ == '__main__':
     globals()[sys.argv[1]](sys.argv[2])
