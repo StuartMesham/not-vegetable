cdfd192fe51e2784f4f0f4311837c5dee51d6f48
Stuart Mesham <stumes3@gmail.com>
2019-04-30 22:05:53 +0200

    initial model test

---
+++ models/model1.py
@@ -0,0 +1,81 @@
+import os
+import datetime
+from keras.preprocessing.image import ImageDataGenerator
+from keras.models import Sequential
+from keras.layers import Conv2D, MaxPooling2D, Activation, Dropout, Flatten, Dense
+from keras.callbacks import TensorBoard
+
+# filthy fix for file reading errors
+from PIL import ImageFile
+ImageFile.LOAD_TRUNCATED_IMAGES = True
+
+IMAGE_SIZE = 150
+BATCH_SIZE = 64
+
+# make new empty log directory for tensorboard
+LOG_DIR = 'data/summaries/model1_' + datetime.datetime.now().strftime("%Y-%m-%d-%Hh%M")
+if os.path.isdir(LOG_DIR):
+    os.rmdir(LOG_DIR)
+os.makedirs(LOG_DIR)
+
+
+strawberry_gen = ImageDataGenerator(
+    validation_split=0.2,
+)
+train_datagen = strawberry_gen.flow_from_directory(
+    'data',
+    target_size=(IMAGE_SIZE, IMAGE_SIZE),
+    batch_size=BATCH_SIZE,
+    classes=['strawberry', 'not-strawberry'],
+    class_mode='binary',
+    subset='training'
+)
+val_datagen = strawberry_gen.flow_from_directory(
+    'data',
+    target_size=(IMAGE_SIZE, IMAGE_SIZE),
+    batch_size=BATCH_SIZE,
+    classes=['strawberry', 'not-strawberry'],
+    class_mode='binary',
+    subset='validation'
+)
+
+print(train_datagen.n)
+print(val_datagen.n)
+
+model = Sequential()
+model.add(Conv2D(32, (3, 3), input_shape=(IMAGE_SIZE, IMAGE_SIZE, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(32, (6, 6), strides=(2, 2)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(64, (3, 3)))
+model.add(Activation('relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(64))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+model.add(Dense(1))
+model.add(Activation('sigmoid'))
+
+model.summary()
+
+model.compile(loss='binary_crossentropy',
+              optimizer='rmsprop',
+              metrics=['accuracy'])
+
+tensorboard = TensorBoard(log_dir=LOG_DIR, histogram_freq=0, write_graph=False, write_images=False)
+
+
+model.fit_generator(
+    train_datagen,
+    steps_per_epoch=2000//BATCH_SIZE,
+    epochs=20,
+    validation_data=val_datagen,
+    validation_steps=800//BATCH_SIZE,
+    callbacks=[tensorboard]
+)
